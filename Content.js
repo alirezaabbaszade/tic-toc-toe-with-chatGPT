b7d47d47a72d9ded322484dc87bcc8157a005f6e
Mohammad Golchin <Golchin.muhammad@gmail.com>
2024-02-29 04:44:59 +0330

    add read gpt last answer

--- Content.js
+++ Content.js
@@ -4,6 +4,10 @@ document.execCommand('insertText', false, 'hi this is a test !!');
 
   setTimeout(function() {
     document.querySelector('[data-testid="send-button"]').click();
-  }, 5000);
-
+    setTimeout(function() {
+      const lastgptResult = [...document.getElementsByClassName('w-full text-token-text-primary')].filter((element,index) => index % 2 != 0).pop();
+      console.log(lastgptResult.getElementsByTagName('p')[0].innerHTML);
+    }, 2000);  //some times request has delay sooo =>> we cant trust to this 2000 mili seconds !!
+  }, 2000);
+  
 }, 5000);
